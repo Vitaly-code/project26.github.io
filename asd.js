353490e3e1c64dcc823a9aeb081713dc35964438
Vitaly-code <61009027+Vitaly-code@users.noreply.github.com>
2022-11-20 20:54:12 +0500

    Add files via upload

---
+++ asd.js
@@ -0,0 +1,81 @@
+alert('ASD');
+function as(){
+var x=10;
+var y=5;
+var asd=52;
+var o=11;
+var t=1.17;
+var result=x+y-(x+o);
+alert(result);
+}
+
+function asq(){
+alert("«р€ ты это нажал, это не надо было нажимать");
+}
+
+function openWin() {
+myWin=open("1.html", "newWindow", status="no");
+}
+
+var milisec = 0;
+  var seconds = 0;
+  document.d.d2.value = '0';
+  
+  function display()
+  {
+  
+    if (milisec >= 9)
+    {
+      milisec = 0;
+      seconds += 1;
+    }
+    else milisec += 1;
+    document.d.d2.value = seconds + "." + milisec;
+    setTimeout("display()", 100);
+  }
+
+  display();
+
+function OpenHelloWnd()
+    {
+      var wndNewWindow;
+      wndNewWindow = window.open("1.html", "", 
+        "toolbar=no,menubar=no,scrollbars=no,width=500,height=200");
+    }
+
+function op() 
+     {
+	   var text = document.getElementById("text");
+	   if(text.style.display == 'none')
+          {
+		    text.style.display = 'block';
+			}
+       else 
+	      {
+		  text.style.display = 'none';
+		  }
+    }
+function cl() 
+{
+ var text=document.getElementById("text");
+ text.style.display = 'none';
+ }
+ var m = new Array(1,35,'Hello');
+ m[0] = 'Pay';
+ m[1] =35;
+ m[2] = 56;
+ function f()
+{
+  for(i=0;i<3;i++) 
+   alert(m[i]);
+   
+var m = new Array(1,35,'Hello');
+ m[0] = 'Pay';
+ m[1] =35;
+ m[2] = 56;
+}
+
+
+
+
+
